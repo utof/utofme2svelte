fc9100d85dbbd7378473b064f0e53d577900f38e
utof <bondar030@mail.ru>
2025-02-03 21:36:43 +0300

    test deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      pages: "build",
-      assets: "build",
       fallback: "404.html",
     }),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
     },
   },
-  preprocess: vitePreprocess(),
 };
 
 export default config;
