944ee061232a6b37fd2ff0f820ca143ced8b6ac3
utof <bondar030@mail.ru>
2025-02-10 16:27:44 +0300

    add @tabler/icons-svelte; soundcloud card (logo not working for some reason)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,22 @@
 <script>
   import Card from '$lib/components/ui/card/Card.svelte';
-  import placeholder1 from '$lib/components/tg.svg';
-  import placeholder2 from '$lib/components/ig.svg';
+  import telegram from '$lib/components/tg.svg';
+  import instagram from '$lib/components/ig.svg';
+  import { IconBrandSoundcloud } from '@tabler/icons-svelte';
+  import { fade } from 'svelte/transition';
 
   const links = [
-    { logo: placeholder1, username: 'utofy', link: 'https://lemreny.t.me' },
-    { logo: placeholder2, username: 'utofy', link: 'https://instagram.com/utofy' }
+    { logo: telegram, username: 'utofy', link: 'https://utofy.t.me' },
+    { logo: instagram, username: 'utofy', link: 'https://instagram.com/utofy' },
+    { logo: IconBrandSoundcloud, username: 'utof', link: 'https://soundcloud.com/utof' },
+
   ];
 </script>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4" transition:fade={{ duration: 500 }}>
   {#each links as { logo, username, link }}
-    <Card {logo} {username} {link} />
+	<Card {logo} {username} link={link} on:click={() => openLink(link)} />
   {/each}
-  <!-- link to a test page -->
   <div class="col-span-full text-center mt-4">
     <a href="/test" class="text-blue-500 underline">Go to Test Page</a>
   </div>
