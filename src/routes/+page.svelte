870168cb6444dd3415f469ab7c0f4cb9fce9424f
utof <bondar030@mail.ru>
2025-02-15 03:41:27 +0300

    app.html import app.css dont load page until load styles. very slow loading then. todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,27 @@
 <script>
-  import Card from '$lib/components/ui/card/Card.svelte';
-  import { IconBrandTelegram, IconBrandInstagram, IconBrandSoundcloud, IconHeart } from '@tabler/icons-svelte';
-  import { fade } from 'svelte/transition';
-
-  const links = [
-    { logo: IconBrandTelegram, username: '@utofy', link: 'https://utofy.t.me' },
-    { logo: IconBrandTelegram, username: '@alpoati - my telegram channel', link: 'https://alpoati.t.me'},
-    { logo: IconBrandInstagram, username: '@utofy', link: 'https://instagram.com/utofy' },
-    { logo: IconBrandSoundcloud, username: '@utof', link: 'https://soundcloud.com/utof' },
-    { logo: IconHeart, username: 'Support me on ko-fi!!', link: 'https://ko-fi.com/utofy' },
-
-  ];
-</script>
-
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4" transition:fade={{ duration: 500 }}>
-  {#each links as { logo, username, link }}
-	<Card {logo} {username} link={link} on:click={() => openLink(link)} />
-  {/each}
-  <div class="col-span-full text-center mt-4">
-    <a href="/test" class="text-blue-500 underline">Go to Test Page</a>
+    import Card from '$lib/components/ui/card/Card.svelte';
+    import { IconBrandTelegram, IconBrandInstagram, IconBrandSoundcloud, IconHeart } from '@tabler/icons-svelte';
+    import { fade } from 'svelte/transition';
+  
+    const links = [
+      { logo: IconBrandTelegram, username: '@utofy', link: 'https://utofy.t.me' },
+      { logo: IconBrandTelegram, username: '@alpoati - my telegram channel', link: 'https://alpoati.t.me'},
+      { logo: IconBrandInstagram, username: '@utofy', link: 'https://instagram.com/utofy' },
+      { logo: IconBrandSoundcloud, username: '@utof', link: 'https://soundcloud.com/utof' },
+      { logo: IconHeart, username: 'Support me on ko-fi!!', link: 'https://ko-fi.com/utofy' },
+  
+    ];
+  </script>
+  
+  <div class="grid gap-4 p-4" transition:fade={{ duration: 500 }}>
+    {#each links as { logo, username, link }}
+      <Card {logo} {username} link={link} on:click={() => openLink(link)} />
+    {/each}
+    <div class="col-span-full text-center mt-4">
+      <a href="/test" class="text-blue-500 underline">Go to Test Page</a>
+    </div>
+    <div class="col-span-full text-center mt-4">
+      <a href="/wishlist" class="text-blue-500 underline">Go to Wishlist Page</a>
+    </div>
   </div>
-  <div class="col-span-full text-center mt-4">
-    <a href="/wishlist" class="text-blue-500 underline">Go to Wishlist Page</a>
-  </div>
-</div>
+  
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173, // or your preferred port
+  },
 });
