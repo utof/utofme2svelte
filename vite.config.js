49d8b5b083a11c841fcf96e946896833d9e4fa2a
utof <bondar030@mail.ru>
2025-02-15 04:03:45 +0300

    delete host rules. no mo 127 0 0 1

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
-  server: {
-    host: "127.0.0.1",
-    port: 5173, // or your preferred port
-  },
 });
